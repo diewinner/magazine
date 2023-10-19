eabd283c8e843eb6507a74d9eb4470d20556ff35
Ilnur <112321302+diewinner@users.noreply.github.com>
2023-10-19 13:02:48 +0300

    refactoring header

--- src/desktop/components/header.jsx
+++ src/desktop/components/header.jsx
@@ -1,9 +1,9 @@
 import logo from "../assets/img/logo.png";
-import searchImage from "../assets/img/search.png";
 import favoriteImage from "../assets/img/heart.png";
 import cartImage from "../assets/img/cart.png";
 
 export const Header = () => {
+    const navigation = ['home', 'shop']
   return (
       <header className={'d_header_container'}>
           <div className={'d_header_logo'}>
@@ -11,12 +11,12 @@ export const Header = () => {
           </div>
           <nav className={'d_nav_container'}>
               <ul className={'d_nav_container__navigation'}>
-                  <li className={'d_nav_container__navigation__item'}>HOME</li>
-                  <li className={'d_nav_container__navigation__item'}>SHOP</li>
+                  {navigation.map((item) => (
+                      <li className={'d_nav_container__navigation__item'}>{item.toUpperCase()}</li>
+                  ))}
               </ul>
           </nav>
           <div className={'d_nav_container__btns'}>
-              <div className={'d_nav_container__btns__item'}><img src={searchImage} alt={'searchImg'}/></div>
               <div className={'d_nav_container__btns__item'}><img src={favoriteImage} alt={'favoriteImg'}/></div>
               <div className={'d_nav_container__btns__item'}><img src={cartImage} alt={'cartImg'}/></div>
           </div>
