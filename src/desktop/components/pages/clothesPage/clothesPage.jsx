145d4488a4650dbd0d7bd8025e47555acb723652
Ilnur <112321302+diewinner@users.noreply.github.com>
2023-10-21 16:22:26 +0300

    refactor code

--- src/desktop/components/pages/clothesPage/clothesPage.jsx
+++ src/desktop/components/pages/clothesPage/clothesPage.jsx
@@ -1,24 +1,28 @@
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {HeartFavoriteSvg} from "../../../assets/img/svg/heartFavorite_icon";
 import {CartSvg} from "../../../assets/img/svg/cart_icon";
-import {useDispatch, useSelector} from "react-redux";
 import {addCustomerAction, deleteCustomerAction} from "../../../../global/store/favoriteReducer";
 
 
 export const ClothesPage = ({title, description, clothes}) => {
 
+    const [clickedItems, setClickedItems] = useState({});
+
     const dispatch = useDispatch()
 
     const favorite = useSelector(state => state.favorite.favorite)
-
     function favoriteBtn(cloth) {
         if(favorite.includes(cloth)) {
             dispatch(deleteCustomerAction(cloth.id))
+            setClickedItems({ ...clickedItems, [cloth.id]: false });
         } else {
             dispatch(addCustomerAction(cloth))
+            setClickedItems({ ...clickedItems, [cloth.id]: true });
         }
     }
 
-    console.log(favorite)
+    console.log(favorite, clickedItems)
   return (
       <div className={'d_clothes_container'}>
           <div className={'d_clothes_title_container'}>
@@ -30,7 +34,12 @@ export const ClothesPage = ({title, description, clothes}) => {
                   return (
                       <div key={item.id} id={'item'} className={'d_clothes_info_item'}>
                           <div className={'d_clothes_info_item__svg'}>
-                              <div className={'d_clothes_info_item__svg__favorite'} onClick={() => favoriteBtn(item)}>
+                              <div className={clickedItems[item.id]
+                                             ? 'd_clothes_info_item__svg__favorite__active'
+                                             : 'd_clothes_info_item__svg__favorite'
+                                             }
+                                   onClick={() => favoriteBtn(item)}
+                              >
                                   <HeartFavoriteSvg width={25} height={25}/>
                               </div>
                               <div className={'d_clothes_info_item__svg__cart'}>
